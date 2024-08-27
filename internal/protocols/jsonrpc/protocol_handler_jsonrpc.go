3b5d5c07f56864e407edc76cfc73ac3d46a31856
Javier Guerra <javier.guerra@konghq.com>
2024-08-27 13:37:54 -0500

    simplify ticker use

--- internal/protocols/jsonrpc/protocol_handler_jsonrpc.go
+++ internal/protocols/jsonrpc/protocol_handler_jsonrpc.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"time"
 
 	"github.com/Kong/go-openrpc/runtime"
 	"github.com/Kong/go-openrpc/websocket"
@@ -34,7 +33,6 @@ import (
 	"github.com/mikefero/kheper/internal/tick"
 	"github.com/mikefero/kheper/node"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
@@ -49,15 +47,11 @@ type HandlerBuilder struct {
 }
 
 func (b *HandlerBuilder) Build(opts node.ProtocolHandlerBuildOpts) (node.ProtocolHandler, error) {
-
 	handler := &protocolHandlerJSONRPC{
 		db:             opts.Db,
 		logger:         opts.Logger,
 		nodeInfo:       opts.NodeInfo,
 		numConnections: b.Node.NumConnections,
-		pingInterval:   b.Globals.PingInterval,
-		pingJitter:     b.Globals.PingJitter,
-		attributes:     opts.Attributes,
 		metrics:        opts.Metrics,
 		serverURL: url.URL{
 			Scheme: "wss",
@@ -76,6 +70,12 @@ func (b *HandlerBuilder) Build(opts node.ProtocolHandlerBuildOpts) (node.Protoco
 		},
 		capabilityNames: b.Node.Capabilities,
 		methodStore:     store.NewMethodStore(opts.Db, opts.NodeInfo.ID),
+		ticker: tick.Ticker{
+			Interval: b.Globals.PingInterval,
+			Jitter:   b.Globals.PingJitter,
+			Logger:   opts.Logger,
+			Tracer:   otel.Tracer("", trace.WithInstrumentationAttributes(opts.Attributes...)),
+		},
 	}
 
 	return handler, nil
@@ -87,17 +87,15 @@ type protocolHandlerJSONRPC struct {
 	serverURL      url.URL
 	nodeInfo       node.Info
 	numConnections int
-	pingInterval   time.Duration
-	pingJitter     time.Duration
 
-	attributes []attribute.KeyValue
-	metrics    *monitoring.Monitoring
+	metrics *monitoring.Monitoring
 
 	methodStore store.MethodStore
 
 	dialer          gorillaWebsocket.Dialer
 	connections     []*runtime.Conn
 	capabilityNames []string
+	ticker          tick.Ticker
 }
 
 func (s *protocolHandlerJSONRPC) IsConnected() bool {
@@ -105,7 +103,6 @@ func (s *protocolHandlerJSONRPC) IsConnected() bool {
 }
 
 func (s *protocolHandlerJSONRPC) Run(ctx context.Context) error {
-
 	capabilitiesHeader, err := json.Marshal(s.capabilityNames)
 	if err != nil {
 		return err
@@ -132,20 +129,9 @@ func (s *protocolHandlerJSONRPC) Run(ctx context.Context) error {
 			return err
 		}
 	}
-
-	s.startTicker()
+	s.ticker.Start(ctx, s.ping)
 
 	return nil
-}
-
-func (s *protocolHandlerJSONRPC) startTicker() {
-	(&tick.Ticker{
-		Interval: s.pingInterval,
-		Jitter:   s.pingJitter,
-		Logger:   s.logger,
-		Tracer:   otel.Tracer("", trace.WithInstrumentationAttributes(s.attributes...)),
-	}).Start(context.Background(), s.ping)
-
 }
 
 func (s *protocolHandlerJSONRPC) registerCapabilities(conn *runtime.Conn, capabilitiesHeader string) error {
@@ -175,7 +161,6 @@ func (s *protocolHandlerJSONRPC) ping(ctx context.Context) {
 		Version: s.nodeInfo.Version.String(),
 	}
 	resp, err := kong_sync.CallGetDelta(ctx, s.connections[0], &params)
-
 	if err != nil {
 		s.methodStore.RecordErrorResponse(&params, err)
 		return
